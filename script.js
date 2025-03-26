bf327a2ad58bf0aa62a11a38a988589de6bcf1c5
Patolll <patryk_dmochowski@wp.pl>
2025-03-26 23:03:35 +0100

    basic logic

--- script.js
+++ script.js
@@ -8,7 +8,16 @@ let snake = [
   { x: 180, y: 200 },
   { x: 160, y: 200 },
 ];
-
+const eyeSize = 5;
+const eyeSize2 = 2;
+let direction = { x: 1, y: 0 };
+let apple = {
+  x: Math.floor((Math.random() * canvas.width) / tileSize) * tileSize,
+  y: Math.floor((Math.random() * canvas.height) / tileSize) * tileSize,
+};
+let gameInterval;
+let points = 0;
+let resultPoints = document.getElementById("points");
 function drawGrid() {
   ctx.strokeStyle = "black";
   for (let i = 0; i <= cols; i++) {
@@ -33,37 +42,113 @@ function drawSnake() {
     ctx.fill();
   });
 
-  const head = snake[0];
-  const eyeSize = 5;
-  const eyeSize2 = 2;
-
   ctx.fillStyle = "black";
   ctx.beginPath();
-  ctx.arc(head.x + 28, head.y + 10, eyeSize, 0, Math.PI * 2);
+  ctx.arc(snake[0].x + 28, snake[0].y + 10, eyeSize, 0, Math.PI * 2);
   ctx.fill();
 
   ctx.beginPath();
-  ctx.arc(head.x + 28, head.y + 30, eyeSize, 0, Math.PI * 2);
+  ctx.arc(snake[0].x + 28, snake[0].y + 30, eyeSize, 0, Math.PI * 2);
   ctx.fill();
 
   ctx.fillStyle = "white";
   ctx.beginPath();
-  ctx.arc(head.x + 31, head.y + 10, eyeSize2, 0, Math.PI * 2);
+  ctx.arc(snake[0].x + 31, snake[0].y + 10, eyeSize2, 0, Math.PI * 2);
   ctx.fill();
 
   ctx.beginPath();
-  ctx.arc(head.x + 31, head.y + 30, eyeSize2, 0, Math.PI * 2);
+  ctx.arc(snake[0].x + 31, snake[0].y + 30, eyeSize2, 0, Math.PI * 2);
   ctx.fill();
 
   ctx.strokeStyle = "red";
   ctx.lineWidth = 3;
 
   ctx.beginPath();
-  ctx.moveTo(head.x + tileSize, head.y + tileSize / 2);
-  ctx.lineTo(head.x + tileSize + 15, head.y + tileSize / 2 - 3);
-  ctx.moveTo(head.x + tileSize, head.y + tileSize / 2);
-  ctx.lineTo(head.x + tileSize + 15, head.y + tileSize / 2 + 3);
+  ctx.moveTo(snake[0].x + tileSize, snake[0].y + tileSize / 2);
+  ctx.lineTo(snake[0].x + tileSize + 15, snake[0].y + tileSize / 2 - 3);
+  ctx.moveTo(snake[0].x + tileSize, snake[0].y + tileSize / 2);
+  ctx.lineTo(snake[0].x + tileSize + 15, snake[0].y + tileSize / 2 + 3);
   ctx.stroke();
 }
-drawGrid();
-drawSnake();
+function drawApple() {
+  ctx.fillStyle = "red";
+  ctx.fillRect(apple.x, apple.y, tileSize, tileSize);
+}
+function generateApple() {
+  let validPosition = false;
+  while (!validPosition) {
+    apple.x = Math.floor((Math.random() * canvas.width) / tileSize) * tileSize;
+    apple.y = Math.floor((Math.random() * canvas.height) / tileSize) * tileSize;
+
+    validPosition = !snake.some(
+      (segment) => segment.x === apple.x && segment.y === apple.y
+    );
+  }
+}
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawGrid();
+  drawSnake();
+  const head = snake[0];
+  let newHead = {
+    x: head.x + direction.x * tileSize,
+    y: head.y + direction.y * tileSize,
+  };
+  snake.unshift(newHead);
+  if (head.x === apple.x && head.y === apple.y) {
+    generateApple();
+    points++;
+    resultPoints.textContent = `Points: ${points}`;
+  } else {
+    snake.pop();
+  }
+  if (canvasColision()) {
+    return;
+  }
+  drawApple();
+}
+
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      if (direction.y === 0) direction = { x: 0, y: -1 };
+      break;
+    case "ArrowDown":
+      if (direction.y === 0) direction = { x: 0, y: 1 };
+      break;
+    case "ArrowLeft":
+      if (direction.x === 0) direction = { x: -1, y: 0 };
+      break;
+    case "ArrowRight":
+      if (direction.x === 0) direction = { x: 1, y: 0 };
+      break;
+  }
+});
+function canvasColision() {
+  const head = snake;
+  for (let i = 0; i < head.length; i++) {
+    const segment = head[i];
+    if (
+      segment.x >= canvas.width + tileSize ||
+      segment.x < 0 - tileSize ||
+      segment.y >= canvas.height + tileSize ||
+      segment.y < 0 - tileSize
+    ) {
+      stopGame();
+      alert("Koniec");
+      return true;
+    }
+  }
+  return false;
+}
+function startGame() {
+  gameInterval = setInterval(gameLoop, 100);
+}
+function stopGame() {
+  clearInterval(gameInterval);
+}
+startGame();
+segment.x >= canvas.width + tileSize ||
+  segment.x < 0 - tileSize ||
+  segment.y >= canvas.height + tileSize ||
+  segment.y < 0 - tileSize;
